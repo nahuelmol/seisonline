7ba3c23fc6156994f779ad9dc97c94ba7bd375bb
nahuelmol <kindermolina98@gmail.com>
2024-12-01 13:23:56 -0300

    api?

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,25 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("this is manual")
 }
 
+async fn example1() -> impl Responder {
+    HttpResponse::Ok().body("example1")
+}
+
+async fn example2() -> impl Responder {
+    HttpResponse::Ok().body("example2")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .service(
+                web::scope("/api")
+                .route("/example1", web::get().to(example1))
+                .route("/example2", web::get().to(example2)),
+            )
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
